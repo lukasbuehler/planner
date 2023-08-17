db0bc58275958148603b6ef390245f235f4d53b7
Lukas Bühler <me@lukasbuehler.ch>
2023-08-17 12:57:06 +0200

    started with layout

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,10 @@
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Item from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
 
 // dark mode
 import { ThemeProvider, createTheme } from "@mui/material/styles";
@@ -20,12 +23,37 @@ export default function Home() {
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
       <main>
-        <Container>
-          <Box>
-            <Card>
-              <Typography variant="h2">Hello World ~</Typography>
-            </Card>
-          </Box>
+        <Container maxWidth={false}>
+          {" "}
+          {/* no maximum width */}
+          <Grid container spacing={2}>
+            <Grid item sx={{ display: { xl: "block", xs: "none" } }} xl={3}>
+              <Item>
+                <Paper elevation={24}>
+                  <Typography variant="h5">Projects</Typography>
+                </Paper>
+              </Item>
+            </Grid>
+            <Grid item xs={12} md={8} xl={6}>
+              <Item>
+                <Paper elevation={24}>
+                  <Typography variant="h5">Today</Typography>
+                </Paper>
+              </Item>
+            </Grid>
+            <Grid
+              item
+              sx={{ display: { md: "block", xs: "none" } }}
+              md={4}
+              xl={3}
+            >
+              <Item>
+                <Paper elevation={24}>
+                  <Typography variant="h4">Tasks</Typography>
+                </Paper>
+              </Item>
+            </Grid>
+          </Grid>
         </Container>
       </main>
     </ThemeProvider>
